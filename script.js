8a84c8767b002b3dcc6c4c4dc6fd7cd6c4f52c98
fahmiAwan <88267227+fahmiAwan@users.noreply.github.com>
2025-02-16 16:47:22 +0800

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function submitForm(event) {
    event.preventDefault();
    var form = event.target;
    var formData = new FormData(form);
-   var mailtoLink = `mailto:awan2bim@gmail.com?subject=Enquiry Form Submission&body=Name: ${formData.get('name')}%0D%0AEmail: ${formData.get('email')}%0D%0APhone: ${formData.get('phone')}%0D%0AMessage: ${formData.get('message')}`;
+   var mailtoLink = `mailto:awanbimconsultant@hotmail.com?subject=Enquiry Form Submission&body=Name: ${formData.get('name')}%0D%0AEmail: ${formData.get('email')}%0D%0APhone: ${formData.get('phone')}%0D%0AMessage: ${formData.get('message')}`;
    window.location.href = mailtoLink;
    document.getElementById('overlay').style.display = 'none';
 }
